343e7b5b17213a2d835df1e1b1058f1c07ed2e32
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-26 20:18:18 +0100

    Create Status.java

---
+++ backend/src/main/java/backend/biddingwars/model/Status.java
@@ -0,0 +1,16 @@
+package backend.biddingwars.model;
+
+/**
+ * Enum representing the status of an auction item.
+ *
+ * @author Oleander Tengesdal
+ * @version 1.0
+ * @since 26-01-2026
+ */
+public enum Status {
+    ACTIVE,
+    INACTIVE,
+    SOLD,
+    PENDING,
+    ARCHIVED
+}
