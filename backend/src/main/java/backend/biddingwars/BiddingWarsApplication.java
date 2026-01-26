f957b44cd22fc502b47b1507ca0437b02ad14b48
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-26 19:59:27 +0100

    Update BiddingWarsApplication.java

--- backend/src/main/java/backend/biddingwars/BiddingWarsApplication.java
+++ backend/src/main/java/backend/biddingwars/BiddingWarsApplication.java
@@ -1,10 +1,12 @@
 package backend.biddingwars;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * Main application class for Bidding Wars backend.
  */
 public class BiddingWarsApplication {
     public static void main(String[] args) {
-        // comming
+        SpringApplication.run(BiddingWarsApplication.class, args);
     }
 }
