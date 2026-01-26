17150ae33dd6479ba8d38f4dba103c41d8a710bc
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-26 20:16:17 +0100

    Update UserDTO.java

--- backend/src/main/java/backend/biddingwars/dto/UserDTO.java
+++ backend/src/main/java/backend/biddingwars/dto/UserDTO.java
@@ -1,4 +1,19 @@
 package backend.biddingwars.dto;
 
-public class UserDTO {
-}
+import backend.biddingwars.model.Role;
+
+/**
+ * Data Transfer Object for User entity.
+ *
+ * @author Oleander Tengesdal
+ * @version 1.0
+ * @since 26-01-2026
+ */
+public record UserDTO(
+        Long id,
+        String username,
+        String email,
+        String firstName,
+        String lastName,
+        Role role
+) { }
