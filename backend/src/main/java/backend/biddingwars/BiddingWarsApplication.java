3644f9d02582741573bbbef2ec79568e4e17e6d3
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-26 19:27:13 +0100

    Create BiddingWarsApplication.java

---
+++ backend/src/main/java/backend/biddingwars/BiddingWarsApplication.java
@@ -0,0 +1,10 @@
+package backend.biddingwars;
+
+/**
+ * Main application class for Bidding Wars backend.
+ */
+public class BiddingWarsApplication {
+    public static void main(String[] args) {
+        // comming
+    }
+}
