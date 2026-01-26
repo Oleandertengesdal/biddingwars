4a4b0753d5f30e2cb1fae7fefd04af7064570a02
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-26 20:03:05 +0100

    Update javadoc

--- backend/src/main/java/backend/biddingwars/model/Bid.java
+++ backend/src/main/java/backend/biddingwars/model/Bid.java
@@ -1,4 +1,11 @@
 package backend.biddingwars.model;
 
+/**
+ * Represents a bid in the bidding wars application.
+ *
+ * @author Oleander Tengesdal
+ * @version 1.0
+ * @since 26-01-2026
+ */
 public class Bid {
 }
