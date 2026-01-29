f3c133f965544a2ba02577564b5a802a00ca4b5d
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-29 12:16:11 +0100

    Created SecurityConfig

---
+++ backend/src/main/java/backend/biddingwars/config/CorsConfig.java
@@ -0,0 +1,8 @@
+package backend.biddingwars.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+
+}
---
+++ backend/src/main/java/backend/biddingwars/security/JwtUtil.java
@@ -0,0 +1,4 @@
+package backend.biddingwars.security;
+
+public class JwtUtil {
+}
