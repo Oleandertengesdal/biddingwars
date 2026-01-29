77491dea1968f5c3de96bef598272ef75c34f2df
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-01-29 12:43:53 +0100

    Updated the Cors Configuration

--- backend/src/main/java/backend/biddingwars/config/CorsConfig.java
+++ backend/src/main/java/backend/biddingwars/config/CorsConfig.java
@@ -1,8 +1,40 @@
 package backend.biddingwars.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
+
+/**
+ * Cors Configuration to allow connections
+ *
+ * @author oleandertengesdal
+ * @version 1.0
+ * @since 29-01-2026
+ */
 @Configuration
+@EnableWebSecurity
 public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+
+    //TODO:  add Jwtoken support, and cookie support!
 
 }
